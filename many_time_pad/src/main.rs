2ce50a8faa71be6a1ab0007f014f34ab01a11923
nczhu <nicole@parity.io>
2020-02-18 23:26:11 +0100

    ay carumba, finally solved it

--- many_time_pad/src/main.rs
+++ many_time_pad/src/main.rs
@@ -23,10 +23,10 @@ Strategies:
 
 extern crate hex;  // Crate: encodes values into a Vec<u8>
 use hex::*;
-use std::collections::HashMap;
 
 // Set up, target string is [0]
 const ciphers_strings: [&str; 10] = [
+                        //145b... 
     "315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146fb778cdf2d3aff021dfff5b403b510d0d0455468aeb98622b137dae857553ccd8883a7bc37520e06e515d22c954eba5025b8cc57ee59418ce7dc6bc41556bdb36bbca3e8774301fbcaa3b83b220809560987815f65286764703de0f3d524400a19b159610b11ef3e",
     "234c02ecbbfbafa3ed18510abd11fa724fcda2018a1a8342cf064bbde548b12b07df44ba7191d9606ef4081ffde5ad46a5069d9f7f543bedb9c861bf29c7e205132eda9382b0bc2c5c4b45f919cf3a9f1cb74151f6d551f4480c82b2cb24cc5b028aa76eb7b4ab24171ab3cdadb8356f",
     "32510ba9a7b2bba9b8005d43a304b5714cc0bb0c8a34884dd91304b8ad40b62b07df44ba6e9d8a2368e51d04e0e7b207b70b9b8261112bacb6c866a232dfe257527dc29398f5f3251a0d47e503c66e935de81230b59b7afb5f41afa8d661cb",
@@ -42,37 +42,85 @@ const ciphers_strings: [&str; 10] = [
 // 83 chars
 const message_string: &str = "32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904";
 
+const len: usize = 83; // hacky but makes hacking easier
+
 fn main() {
     // converts strings into hex
     let mut ciphers: Vec<Vec<u8>> = ciphers_strings.into_iter().map( |x| Vec::from_hex(x).unwrap() ).collect();
-    let mut message: Vec<u8> = Vec::from_hex(message_string).unwrap();
-
-    let mut XOR_vector: Vec<Vec<u8>> = Vec::new();
+    let message: Vec<u8> = Vec::from_hex(message_string).unwrap();
     
-    for i in 0..10 {
-        ciphers[i].truncate(message.len()); // truncate cipher to same length as message
+    let mut xor_vector: Vec<Vec<u8>> = Vec::new();
+    
+    for i in 0..ciphers.len() {
+        ciphers[i].truncate(len); // truncate cipher to same length as message
         let cipher_xor_message: Vec<u8> = ciphers[i].iter().zip(&message).map(|(x1, x2)| x1 ^ x2).collect();
-        XOR_vector.push(cipher_xor_message);
+        xor_vector.push(cipher_xor_message);
     }
     
                             // position
     // let mut possible_chars: HashMap<usize, char> = HashMap::new();
-    let mut possible_message: Vec<char> = vec!['-'; message.len()];
+    let mut possible_message: Vec<char> = vec!['-'; len];
 
-    // iterate through XOR_vector 
-    for i in 0..10 {
-        for j in 0..message.len() {     // char position
-            if XOR_vector[i][j] >= 65 { // then it's either space or a char at pos j
-                let c = char::from(32 ^ XOR_vector[i][j]); // possible char contender
-
-                // if space isn't occupied yet, replace with c
+    // iterate through xor_vector 
+    for i in 0..ciphers.len() {
+        for j in 0..len {     // char position
+            if xor_vector[i][j] >= 65 { // then it's either space or a char at pos j
+                let c = char::from(32 ^ xor_vector[i][j]); // possible char contender
                 if possible_message[j] == '-' { possible_message[j] = c; }
+                else if possible_message[j] == ' ' {}
                 else if c != possible_message[j] { possible_message[j] = ' '; }
-                // if new char a different char, replace with space
-                // else { possible_message[i] = ' '; }
             }
         }
     }
 
-    print!("\n\n Message is: {:?}", possible_message);
+    // Guessing the key to solve other puzzles to get the first one 100 % accurately
+
+    // print!("\n\n Message is: {:?}", possible_message);
+    let m0: Vec<u8> = "The secret message is  When using a stream cipher, never use the key more than once".chars().map( |x| x as u8 ).collect(); 
+    let key: Vec<u8> = message.iter().zip(&m0).map(|(x1, x2)| x1 ^ x2).collect(); 
+    println!("KEY is: {:?} ", key);
+    
+    for i in 0..ciphers.len() {
+        let phrase: Vec<char> = ciphers[i].iter().zip(&key).map(|(x1, x2)| (x1 ^ x2) as char).collect(); 
+        // println!("{:?}\n", phrase);
+    }
+
+    // another iteration: 
+    let c1: Vec<u8> = "The ciphertext produced by a weak encryption algorithm looks as good as ciphertext ".chars().map( |x| x as u8 ).collect(); 
+    let key: Vec<u8> = ciphers[3].iter().zip(&c1).map(|(x1, x2)| x1 ^ x2).collect(); 
+
+    for i in 0..ciphers.len() {
+        let phrase: Vec<char> = ciphers[i].iter().zip(&key).map(|(x1, x2)| (x1 ^ x2) as char).collect(); 
+        // println!("{:?}\n", phrase);
+    }
+
+    let final_phrase: Vec<char> = message.iter().zip(&key).map(|(x1, x2)| (x1 ^ x2) as char).collect(); 
+    println!("{:?}\n", final_phrase);
+    
 }
+
+/*
+ThecsecuettmessagenisxeWht{tusingqaa~treamocipher\u{7f}cneverauselthefkeyomoreethanbonct
+ThecsecuettmessagenisxeWht{tusingqaa~treamocipher-cneverauselthefkeyomoreethanbonct
+Th  secuet mes age is  Wht  us       tr    cipher  nev r use the key more than on  
+The secret message is _____ using a stream cipher_ never use the key more than once
+The secret message is_ When using a stream cipher, never use the key more than once
+
+My best guess: 
+The secret message is? When using a stream cipher, never use the key more than once
+
+Solve for key, get other sentences: 
+We can factor the numxer 15 with quantum computers. We can also factor the number 1
+Euler would probably \u{7f}njoy that now his theorem becomes a corner stone of crypto - 
+The nice thing about Qeeyloq is now we cryptographers can drive a lot of fancy cars
+The ciphertext produc\u{7f}d by a weak encryption algorithm looks as good as ciphertext **** best one
+You don\'t want to buy:a set of car keys from a guy who specializes in stealing cars
+There are two types o| cryptography - that which will keep secrets safe from your l
+There are two types o| cyptography: one that allows the Government to use brute for
+We can see the point mhere the chip is unhappy if a wrong bit is sent and consumes 
+A (private-key)  encrcption scheme states 3 algorithms, namely a procedure for gene
+ The Concise OxfordDiytionary (2006) deï¬\u{81}nes crypto as the art of  writing o r sol
+
+
+Final answer: The secret message is: When using a stream cipher, never use the key more than once
+*/
